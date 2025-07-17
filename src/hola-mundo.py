f5e9dbb92b5417819678c7cee4741c1faceab973
Valentina Aristizabal <valentina.aristizabale@upb.edu.co>
2025-07-17 11:03:38 -0500

    Creamos la carpeta src y agregamos el archivo hola-mundo.py

---
+++ src/hola-mundo.py
@@ -0,0 +1,10 @@
+# mi_primer_script.py
+print("¡Hola, GitHub!")
+for i in range(5):
+    print(f"Este es el mensaje número {i + 1}")
+print("¡Adiós, GitHub!")
+
+a=10
+while a < 10:
+    print("El valor de a es:", a)
+    a += 1
